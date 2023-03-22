726b0ae9e7a0a9f74f90a74cec3a407666610d5a
Dustin Gouner <dustingouner20@gmail.com>
2023-03-21 20:46:46 -0600

    Completed test and code for turn and card class

---
+++ src/Turn.js
@@ -0,0 +1,29 @@
+class Turn {
+  constructor(cardObj, userGuess) {
+    this.card = cardObj
+    this.guess = userGuess
+  }
+  returnGuess() {
+    return this.guess
+  }
+  returnCard() {
+    return this.card
+  }
+  evaluateGuess() {
+    if (this.guess === this.card.correctAnswer) {
+      return true
+    } else {
+      return false 
+    }
+  }
+  giveFeedback() {
+    if (this.evaluateGuess()) {
+      return 'correct!'
+    } else {
+      return 'incorrect!'
+    }
+  }
+}
+
+
+module.exports = Turn
