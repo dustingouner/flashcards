16f7fc4983b9394730054d5f132c322ea613c722
Dustin Gouner <dustingouner20@gmail.com>
2023-03-21 22:54:51 -0600

    pass test to go to next card after turn

--- src/Round.js
+++ src/Round.js
@@ -1,12 +1,24 @@
+const Turn = require('../src/Turn')
+
 class Round {
   constructor(deckObj) {
     this.deck = deckObj
     this.currentCard = deckObj.cards[0]
+    this.turns = 0
 
   }
   returnCurrentCard() {
     // console.log(this.currentCard)
     return this.currentCard
+  }
+  takeTurn(userGuess) {
+    let turn = new Turn(this.currentCard, userGuess)
+    this.currentCard = this.deck.cards[`${this.turns}`]
+    this.turns += 1
+    
+    
+
+
   }
 }
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 
 const Card = require('../src/Card');
+const Turn = require('../src/Turn');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 
@@ -12,18 +13,58 @@ describe('Round', function() {
     console.log(`Test ${testNum}`)
   });
 
-  it('should be a function', function() {
+  it.skip('should be a function', function() {
     expect(Round).to.be.a('function');
   });
 
-  it('should return the current card', function() {
+  it.skip('should return the current card', function() {
     const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
     const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
     const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
     const deck = new Deck([card1, card2, card3]);
     const round = new Round(deck);
     expect(round.returnCurrentCard()).to.equal(deck.cards[0])
+  });
 
+  it.skip('should be able to keep track of the number of turns', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    expect(round.turns).to.equal(0)
+  });
 
+  it.skip('should increase the number of turns by one each time a turn occurs', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    let turn = new Turn('pug', card1)
+    let turn2 = new Turn('capybara', card2)
+    round.takeTurn(turn.guess)
+    expect(round.turns).to.equal(1)
+    round.takeTurn(turn2.guess)
+    expect(round.turns).to.equal(2)
+  });
+
+  it('should go to the next card after a turn is taken', function() {
+    const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+    const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+    const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+    const deck = new Deck([card1, card2, card3]);
+    const round = new Round(deck);
+    let turn = new Turn('pug', card1)
+    let turn2 = new Turn('gallbladder', card2)
+    let turn3 = new Turn('William', card3)
+    round.takeTurn(turn.guess)
+    expect(round.returnCurrentCard()).to.equal(card1)
+    round.takeTurn(turn2.guess)
+    expect(round.returnCurrentCard()).to.equal(card2)
+    round.takeTurn(turn3.guess)
+    expect(round.returnCurrentCard()).to.equal(card3)
   })
+
+
 })
